81e986a2e05eecd0414fa778dc6702bfd0ff6d13
hyonna12 <hyonna01@gmail.com>
2022-11-21 09:32:36 +0900

    config 수정

--- src/main/java/site/metacoding/second/config/WebSocketConfig.java
+++ src/main/java/site/metacoding/second/config/WebSocketConfig.java
@@ -2,16 +2,11 @@ package site.metacoding.second.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import site.metacoding.second.handler.WebSocketHandler;
 
 // handler를 이용하여 websocket을 활성화하기 위한 config
 @Slf4j
@@ -21,15 +16,15 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
-    registry.addEndpoint("/ws-swoomi")
+    registry.addEndpoint("/ws-swoomi") // stomp websocket의 연결 endpoint
         .setAllowedOriginPatterns("*")
-        .withSockJS();
+        .withSockJS(); // 클라이언트와의 연결을 sockJs로
   }
 
   @Override
   public void configureMessageBroker(MessageBrokerRegistry registry) {
-    registry.enableSimpleBroker("/sub");
-    registry.setApplicationDestinationPrefixes("/pub");
+    registry.enableSimpleBroker("/sub"); // 메시지를 구독하는 요청의 prefix
+    registry.setApplicationDestinationPrefixes("/pub"); // 메시지를 발행하는 요청의 prefix
   }
 
 }
