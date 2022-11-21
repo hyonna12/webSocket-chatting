60fd4d9f13555a28cc4a6907f6d49077548170e6
hyonna12 <hyonna01@gmail.com>
2022-11-21 09:58:20 +0900

    msgController 수정 - publisher 구현

--- src/main/java/site/metacoding/second/web/MsgController.java
+++ src/main/java/site/metacoding/second/web/MsgController.java
@@ -2,6 +2,8 @@ package site.metacoding.second.web;
 
 import java.util.List;
 
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,24 +11,24 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
+import site.metacoding.second.domain.Message;
 import site.metacoding.second.domain.MsgRoom;
 import site.metacoding.second.service.MsgService;
 
 // 채팅방의 생성 및 조회는 rest api로 구현
+// publisher 구현
+// 채팅방은 클라이언트가 sub하고 있는 topic이며 해당방을 sub하는 모든 subscriber에게 채팅 보내줌
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/chat")
 public class MsgController {
 
-  private final MsgService msgService;
+  private final SimpMessageSendingOperations sendingOperations;
 
-  @PostMapping
-  public MsgRoom createRoom(@RequestParam String name) {
-    return msgService.createRoom(name);
-  }
-
-  @GetMapping
-  public List<MsgRoom> findAllRoom() {
-    return msgService.findAllRoom();
+  @MessageMapping("/comm/message")
+  public void message(Message message) {
+    if (Message.MessageType.ENTER.equals(message.getMessageType())) {
+      message.setMessage(message.getSender() + "이 입장했습니다.");
+    }
+    sendingOperations.convertAndSend("/sub/comm/room/" + message.getRoomId(), message);
   }
 }
