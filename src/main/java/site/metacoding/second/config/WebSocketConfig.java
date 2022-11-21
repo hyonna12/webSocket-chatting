9251c1c7c0826c5ce5ebacc2852ae6ff3e3b57d4
hyonna12 <hyonna01@gmail.com>
2022-11-21 09:29:10 +0900

    config 수정 - stomp 이용하기 위해

--- src/main/java/site/metacoding/second/config/WebSocketConfig.java
+++ src/main/java/site/metacoding/second/config/WebSocketConfig.java
@@ -1,11 +1,13 @@
 package site.metacoding.second.config;
 
-import javax.websocket.WebSocketContainer;
-
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,17 +16,20 @@ import site.metacoding.second.handler.WebSocketHandler;
 // handler를 이용하여 websocket을 활성화하기 위한 config
 @Slf4j
 @Configuration
-@RequiredArgsConstructor
-@EnableWebSocket // webcket 활성화
-public class WebSocketConfig implements WebSocketConfigurer {
-
-  private final WebSocketHandler webSocketHandler;
+@EnableWebSocketMessageBroker // stomp 사용하려고
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
   @Override
-  public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-    registry.addHandler(webSocketHandler, "/ws/chat").setAllowedOrigins("*");
-    // websocket에 접속하기 위한 endpoint는 /ws/chat로 설정하고
-    // 도메인이 다른 서버에서도 접속 가능하도록 "*" 를 설정에 추가
+  public void registerStompEndpoints(StompEndpointRegistry registry) {
+    registry.addEndpoint("/ws-swoomi")
+        .setAllowedOriginPatterns("*")
+        .withSockJS();
+  }
+
+  @Override
+  public void configureMessageBroker(MessageBrokerRegistry registry) {
+    registry.enableSimpleBroker("/sub");
+    registry.setApplicationDestinationPrefixes("/pub");
   }
 
 }
