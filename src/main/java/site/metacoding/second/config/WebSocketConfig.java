8c02ec3c3cd7c373e83322b3bacdea154c57d6cf
hyonna12 <hyonna12@gmail.com>
2022-11-23 01:13:39 +0900

    config 수정

--- src/main/java/site/metacoding/second/config/WebSocketConfig.java
+++ src/main/java/site/metacoding/second/config/WebSocketConfig.java
@@ -1,26 +1,26 @@
 package site.metacoding.second.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
-
-import lombok.RequiredArgsConstructor;
-import site.metacoding.second.handler.WebSocketHandler;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 // handler를 이용하여 websocket을 활성화하기 위한 config
 @Configuration
-@RequiredArgsConstructor
-@EnableWebSocket // webcket 활성화
-public class WebSocketConfig implements WebSocketConfigurer {
-
-  private final WebSocketHandler webSocketHandler;
-
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
   @Override
-  public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-    registry.addHandler(webSocketHandler, "/ws/chat").setAllowedOrigins("*");
-    // websocket에 접속하기 위한 endpoint는 /ws/chat로 설정하고
-    // 도메인이 다른 서버에서도 접속 가능하도록 "*" 를 설정에 추가
+  public void configureMessageBroker(MessageBrokerRegistry config) {
+    config.enableSimpleBroker("/sub");
+    config.setApplicationDestinationPrefixes("/pub");
   }
 
+  @Override
+  public void registerStompEndpoints(StompEndpointRegistry registry) {
+    registry.addEndpoint("/ws-stomp").setAllowedOrigins("*")
+        .withSockJS();
+    // websocket에 접속하기 위한 endpoint는 /ws-stomp로 설정하고
+    // 도메인이 다른 서버에서도 접속 가능하도록 "*" 를 설정에 추가
+  }
 }
